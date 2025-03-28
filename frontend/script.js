88eb1900bcd89faeaf7225775e8ae89239aecc74
johnlerner <joaopedrolernercosta@gmail.com>
2025-03-28 00:28:41 -0300

    Ajuste do fetch do frontend para sair do localhost

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/dados')
+fetch(`${window.location.origin}/dados`)
     .then(res => res.json())
     .then(data => {
         const head = document.getElementById('tabela-head');
