d8341b1c2bed93136255ecc7fad1e4d93dc47b33
johnlerner <joaopedrolernercosta@gmail.com>
2025-07-10 19:17:50 -0300

    criação de lógica de migrations

---
+++ backend/.node-pg-migrate.js
@@ -0,0 +1,6 @@
+module.exports = {
+  migrationFolder: 'backend/migrations',
+  direction: 'up',
+  sqlFile: false,
+  databaseUrl: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+};
