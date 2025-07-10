9742e209f5c1996fae8ffebb604e5c6ace18184d
johnlerner <joaopedrolernercosta@gmail.com>
2025-07-10 19:57:35 -0300

    ajuste

--- backend/migrations/1752121324827_create-tabela-categoria.js
+++ backend/migrations/1752121324827_create-tabela-categoria.js
@@ -9,14 +9,41 @@ exports.shorthands = undefined;
  * @returns {Promise<void> | void}
  */
 
-exports.up = pgm => {
-  pgm.createTable('categoria', {
-    id: 'id', // shorthand para SERIAL PRIMARY KEY
-    descricao: {
-      type: 'text',
-      notNull: true
-    }
-  });
+exports.up = (pgm) => {
+  pgm.sql(`
+    CREATE TABLE tarefa (
+    id SERIAL PRIMARY KEY,
+     descricao VARCHAR(255) NOT NULL,
+     situacao VARCHAR(30) NOT NULL,
+     data_criacao DATE NOT NULL,
+     data_prevista DATE,
+     data_encerramento DATE    
+);
+
+INSERT INTO tarefa (descricao, situacao, data_criacao, data_prevista, data_encerramento) VALUES
+('Estudar PostgreSQL', 'Concluído', '2021-01-01', '2021-01-10', NULL),
+('Estudar Node.js', 'Concluído', '2021-01-01', '2021-01-10', NULL),
+('Estudar React', 'Concluído', '2021-01-01', '2021-01-10', NULL),
+('Estudar Docker', 'Em andamento', '2021-01-01', '2021-01-10', NULL),
+('Estudar Kubernetes', 'Em andamento', '2021-01-01', '2021-01-10', NULL),
+('Estudar AWS', 'Pendente', '2021-01-01', '2021-01-10', NULL),
+('Estudar GCP', 'Concluído', '2021-01-01', '2021-01-10', NULL),
+('Estudar Azure', 'Concluído', '2021-01-01', '2021-01-10', NULL),
+('Estudar Java', 'Concluído', '2021-01-01', '2021-01-10', NULL),
+('Estudar Python', 'Em andamento', '2021-01-01', '2021-01-10', NULL);
+
+CREATE TABLE usuario (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL, 
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO usuario (nome, email, senha)
+VALUES 
+  ('João Pedro', 'admin@exemplo.com', 'admin123');
+  `);
 };
 
 /**
@@ -24,6 +51,7 @@ exports.up = pgm => {
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-exports.down = pgm => {
-  pgm.dropTable('categoria');
+exports.down = (pgm) => {
+  pgm.dropTable('tarefa');
+  pgm.dropTable('usuario');
 };
