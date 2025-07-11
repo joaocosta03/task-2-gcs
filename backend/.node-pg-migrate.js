5a0d8738cadc8cd5c5931aeb7e782651e5bb2f62
johnlerner <joaopedrolernercosta@gmail.com>
2025-07-11 00:54:08 -0300

    ajuste

--- backend/.node-pg-migrate.js
+++ backend/.node-pg-migrate.js
@@ -1,5 +1,5 @@
 module.exports = {
-  migrationFolder: 'backend/migrations',
+  migrationFolder: 'migrations',
   direction: 'up',
   sqlFile: false,
   databaseUrl: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
